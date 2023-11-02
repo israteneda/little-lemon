b3cb17801468d755371a5f49aed46785ae7095b6
Israel Teneda <israteneda@gmail.com>
2023-11-02 17:14:21 -0500

    fixes #2 add test for the initial state structure

--- src/reducers/tables.test.ts
+++ src/reducers/tables.test.ts
@@ -1,6 +1,21 @@
-import { tablesReducer } from './tables';
+import { initialState, tablesReducer } from './tables';
 
 describe('tables reducer', () => {
+  it('should return the initial state', () => {
+    // check the each object in the array has the same properties (label, left, top, state, availableTimes)
+    expect(initialState.tables).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          label: expect.any(String),
+          left: expect.any(Number),
+          top: expect.any(Number),
+          state: expect.any(String),
+          availableTimes: expect.any(Array),
+        }),
+      ]),
+    );
+  });
+
   it('should update available times', () => {
     const state = tablesReducer(
       { tables: [{ label: 'IDR-3', left: 70, top: 20, state: 'available', availableTimes: ['17:00', '18:00', '22:00'] }] },
